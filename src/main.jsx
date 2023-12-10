02865897683537ad652e8475809d893b1776b755
Chyyni <catharina0409@gmail.com>
2023-12-10 17:59:59 +0900

    라우터 기본 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import HomePage from './pages/home/HomePage';
+import ListPage from './pages/list/ListPage';
+import PostPage from './pages/post/PostPage';
+import AnswerPage from './pages/answer/AnswerPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/list',
+    element: <ListPage />,
+  },
+  {
+    path: '/post/{id}',
+    element: <PostPage />,
+  },
+  {
+    path: 'post/{id}/answer',
+    element: <AnswerPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
