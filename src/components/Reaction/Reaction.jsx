536502f9745a5bdf35d7533761bef6f367fee8b5
ccwnc <ccwnc337@gmail.com>
2023-12-13 15:28:08 +0900

    Refactor: onClick props명을 handleButtonClick으로 변경

--- src/components/Reaction/Reaction.jsx
+++ src/components/Reaction/Reaction.jsx
@@ -2,17 +2,17 @@ import thumbsUp from '../../assets/icons/thumbsUp.svg';
 import thumbsDown from '../../assets/icons/thumbsDown.svg';
 import styles from './Reaction.module.css';
 
-export default function Reaction({ onClick, like, dislike }) {
+export default function Reaction({ handleButtonClick, like, dislike }) {
   const likeStatus = like === 0 ? `좋아요` : `좋아요 ${like}`;
   const dislikeStatus = dislike === 0 ? `싫어요` : `싫어요 ${dislike}`;
 
   return (
     <div className={styles.reaction}>
-      <button className={styles.like} onClick={onClick}>
+      <button className={styles.like} onClick={handleButtonClick}>
         <img src={thumbsUp} alt='thumbsUp' />
         {likeStatus}
       </button>
-      <button className={styles.dislike} onClick={onClick}>
+      <button className={styles.dislike} onClick={handleButtonClick}>
         <img src={thumbsDown} alt='thumbsDown' />
         {dislikeStatus}
       </button>
