92c67a1cbc15f6889628519124fa42b61654ef47
ccwnc <ccwnc337@gmail.com>
2023-12-13 13:55:06 +0900

    Feat: 불필요한 기능 및 스타일 삭제

--- src/components/common/ButtonFloating/ButtonFloating.jsx
+++ src/components/common/ButtonFloating/ButtonFloating.jsx
@@ -1,29 +1,9 @@
-import { useState, useEffect } from 'react';
 import styles from './ButtonFloating.module.css';
 
-export default function ButtonFloating({ onClick, type }) {
-  const [buttonText, setButtonText] = useState('');
-
-  useEffect(() => {
-    const handleResize = () => {
-      const changeButtonText =
-        type === 'question' ? (window.innerWidth <= 375 ? '질문 작성' : '질문 작성하기') : '삭제하기';
-
-      setButtonText(changeButtonText);
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [type]);
-
+export default function ButtonFloating({ handleButtonClick }) {
   return (
-    <button className={`${styles.button} ${styles[type]}`} onClick={onClick}>
-      {buttonText}
+    <button className={styles.button} onClick={handleButtonClick}>
+      질문 작성하기
     </button>
   );
 }
