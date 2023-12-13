bc58a92f1de267ebb54aa6b4680657138a3be186
jsw <jeongseongwon1121@gmail.com>
2023-12-13 13:17:24 +0900

    feat: 드롭다운메뉴/정렬 컴포넌트 추가

---
+++ src/components/common/DropdownOrder/DropdownOrder.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import arrowUpImage from '../../../images/icons/arrowUp.svg';
+import arrowDownImage from '../../../images/icons/arrowDown.svg';
+import styles from './DropdownOrder.module.css';
+
+export default function DropdownOrder({ children, List, data }) {
+  const [isDropdownView, setDropdownView] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState(children);
+
+  const handleClickContainer = () => {
+    setDropdownView(!isDropdownView);
+  };
+
+  const handleBlurContainer = () => {
+    setTimeout(() => {
+      setDropdownView(false);
+    }, 200);
+  };
+
+  return (
+    <div className={!isDropdownView ? styles.orderBox : styles.orderBoxOpen} onBlur={handleBlurContainer}>
+      <label onClick={handleClickContainer}>
+        <button className={!isDropdownView ? styles.orderButton : styles.orderButtonOpen}>
+          {selectedOrder}
+          {!isDropdownView ? (
+            <img className={styles.arrowDown} src={arrowDownImage} alt='기본 화살표' />
+          ) : (
+            <img className={styles.arrowUp} src={arrowUpImage} alt='오픈 화살표' />
+          )}
+        </button>
+      </label>
+      {isDropdownView && (
+        <ul className={styles.orderList}>
+          {List.map((li) => (
+            <li
+              className={styles.order}
+              key={li}
+              onClick={() => {
+                setSelectedOrder(li);
+              }}
+            >
+              {li}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
