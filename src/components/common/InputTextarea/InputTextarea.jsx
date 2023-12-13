4f4bd7867efef98657a32f3c5c694cdac9f1cff0
ccwnc <ccwnc337@gmail.com>
2023-12-13 15:15:46 +0900

    Refactor: div 삭제 및 css 속성 수정

--- src/components/common/InputTextarea/InputTextarea.jsx
+++ src/components/common/InputTextarea/InputTextarea.jsx
@@ -1,9 +1,5 @@
 import styles from './InputTextarea.module.css';
 
-export default function InputTextArea() {
-  return (
-    <div>
-      <textarea className={styles.textarea} placeholder='이름을 입력하세요' />
-    </div>
-  );
+export default function InputTextArea(placeholder) {
+  return <textarea className={styles.textarea} placeholder={placeholder} />;
 }
