96d7adbd9185607a8d917c231dcdca5770a3b2f6
jsw <jeongseongwon1121@gmail.com>
2023-12-14 15:26:30 +0900

    Refactor: setTimeout함수 상수 선언, list prop 소문자 변경, label 태그 삭제

--- src/components/common/DropdownKebab/DropdownKebab.jsx
+++ src/components/common/DropdownKebab/DropdownKebab.jsx
@@ -3,9 +3,9 @@ import more from '../../../images/icons/more.svg';
 import editImage from '../../../images/icons/edit.svg';
 import styles from './DropdownKebab.module.css';
 
-export default function DropdownKebab({ List, handleButtonClick }) {
+export default function DropdownKebab({ list, handleButtonClick }) {
   const [isDropdownView, setDropdownView] = useState(false);
-
+  const setTime = 200;
   const handleClickContainer = () => {
     setDropdownView(!isDropdownView);
   };
@@ -13,19 +13,17 @@ export default function DropdownKebab({ List, handleButtonClick }) {
   const handleBlurContainer = () => {
     setTimeout(() => {
       setDropdownView(false);
-    }, 200);
+    }, setTime);
   };
 
   return (
     <div onBlur={handleBlurContainer} className={styles.kebabBox}>
-      <label onClick={handleClickContainer}>
-        <button>
-          <img src={more} alt='케밥버튼 이미지' />
-        </button>
-      </label>
+      <button onClick={handleClickContainer}>
+        <img src={more} alt='케밥버튼 이미지' />
+      </button>
       {isDropdownView && (
         <ul className={styles.buttonList}>
-          {List.map((buttonName) => (
+          {list.map((buttonName) => (
             <li className={styles.button} key={buttonName} onClick={handleButtonClick}>
               <img className={styles.buttonImage} src={editImage} alt='수정버튼 이미지' />
               {buttonName}
