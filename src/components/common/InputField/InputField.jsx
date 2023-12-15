74c285f064110b029b0f4956ed461da3ed94c20a
Chyyni <catharina0409@gmail.com>
2023-12-15 20:58:07 +0900

    feat: 인풋 필드 onChange 이벤트에 이벤트 핸들러 추가

--- src/components/common/InputField/InputField.jsx
+++ src/components/common/InputField/InputField.jsx
@@ -1,10 +1,16 @@
 import styles from './InputField.module.css';
 
-export default function InputField({ defaultText, iconSrc, iconAlt }) {
+export default function InputField({ defaultText, iconSrc, iconAlt, value, handleInputChange }) {
   return (
     <div className={styles.inputContainer}>
       <img className={styles.icon} src={iconSrc} alt={iconAlt} />
-      <input className={styles.input} type='text' placeholder={defaultText} />
+      <input
+        className={styles.input}
+        type='text'
+        placeholder={defaultText}
+        value={value}
+        onChange={handleInputChange}
+      />
     </div>
   );
 }
