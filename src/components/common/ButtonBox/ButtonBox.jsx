b4ffb1ad6500e7fa41fdcdad308702d4d752338a
Chyyni <catharina0409@gmail.com>
2023-12-15 15:07:15 +0900

    refactor: 버튼박스 컴포넌트 disabled 속성 추가

--- src/components/common/ButtonBox/ButtonBox.jsx
+++ src/components/common/ButtonBox/ButtonBox.jsx
@@ -1,5 +1,13 @@
 import styles from './ButtonBox.module.css';
 
-export default function ButtonBox({ children, className }) {
-  return <button className={`${styles.button} ${styles[`${className}`]}`}>{children}</button>;
+export default function ButtonBox({ children, className, text = true, handleButtonClick }) {
+  return (
+    <button
+      className={`${styles.button} ${styles[`${className}`]}`}
+      disabled={text ? false : true}
+      onClick={handleButtonClick}
+    >
+      {children}
+    </button>
+  );
 }
