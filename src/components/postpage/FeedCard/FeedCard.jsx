38fa02b20cb53dbcdca14ec87d62b0f5ecaa852a
ccwnc <ccwnc337@gmail.com>
2023-12-17 19:20:48 +0900

    Feat: FeedCard 컴포넌트 구현

--- src/components/postpage/FeedCard/FeedCard.jsx
+++ src/components/postpage/FeedCard/FeedCard.jsx
@@ -1,13 +1,19 @@
+import Badge from '../../common/Badge/Badge';
+import Question from '../Question/Question';
+import Reaction from '../../common/Reaction/Reaction';
+
 import styles from './FeedCard.module.css';
 
 export default function FeedCard({ data, showKebab }) {
+  const { id, answer, createdAt, content, like, dislike } = data;
+
   return (
-    <div>
-      <Badge />
+    <div className={styles.feedCard}>
+      {answer === null ? <Badge className='inActive' text='미답변' /> : <Badge className='active' text='답변 완료' />}
       {/* {showKebab && <Kebab />} */}
-      <Question />
-      <Answer />
-      <Reaction />
+      <Question createdAt={createdAt} content={content} />
+      {/* <Answer /> */}
+      <Reaction id={id} like={like} dislike={dislike} />
       {/* {showEdit && <Edit />} */}
     </div>
   );
