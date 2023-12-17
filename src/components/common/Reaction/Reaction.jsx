bda6242a273ee0b3d33880802fb9c4ca5881fbd9
ccwnc <ccwnc337@gmail.com>
2023-12-17 19:16:06 +0900

    Feat: Reaction 컴포넌트 수정

--- src/components/common/Reaction/Reaction.jsx
+++ src/components/common/Reaction/Reaction.jsx
@@ -1,18 +1,38 @@
-import thumbsUp from '../../assets/icons/thumbsUp.svg';
-import thumbsDown from '../../assets/icons/thumbsDown.svg';
+import { useState, useEffect } from 'react';
+import { axiosBaseURL } from '../../../apis/axiosBaseURL';
+
+import thumbsUp from '../../../images/icons/thumbsUp.svg';
+import thumbsDown from '../../../images/icons/thumbsDown.svg';
 import styles from './Reaction.module.css';
 
-export default function Reaction({ handleButtonClick, like, dislike }) {
-  const likeStatus = `좋아요 ${like === 0 ? '' : like}`;
-  const dislikeStatus = `싫어요 ${dislike === 0 ? '' : dislike}`;
+export default function Reaction({ id, like, dislike }) {
+  const url = `questions/${id}/reaction/`;
+
+  const [likeCount, setLikeCount] = useState(like);
+  const [dislikeCount, setDislikeCount] = useState(dislike);
+
+  const handleButtonClick = (reactionType) => {
+    const postLikeReaction = async () => {
+      try {
+        const response = await axiosBaseURL.post(url, { type: reactionType });
+        reactionType === 'like' ? setLikeCount(response.data.like) : setDislikeCount(response.data.dislike);
+      } catch {
+        console.error('에러가 발생했습니다!');
+      }
+    };
+    postLikeReaction();
+  };
+
+  const likeStatus = `좋아요 ${likeCount === 0 ? '' : likeCount}`;
+  const dislikeStatus = `싫어요 ${dislikeCount === 0 ? '' : dislikeCount}`;
 
   return (
     <div className={styles.reaction}>
-      <button className={styles.button} onClick={handleButtonClick}>
+      <button className={styles.button} onClick={() => handleButtonClick('like')}>
         <img className={styles.img} src={thumbsUp} alt='thumbsUp' />
         {likeStatus}
       </button>
-      <button className={styles.button} onClick={handleButtonClick}>
+      <button className={styles.button} onClick={() => handleButtonClick('dislike')}>
         <img className={styles.img} src={thumbsDown} alt='thumbsDown' />
         {dislikeStatus}
       </button>
