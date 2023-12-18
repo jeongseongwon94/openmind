8a82b5d9189eb5a012b8ee4ce0bc4a2bb8ae4257
ccwnc <ccwnc337@gmail.com>
2023-12-18 20:56:42 +0900

    Feat: MainSection 및 QuestionBox 구조 수정

--- src/components/postpage/QuestionBox/QuestionBox.jsx
+++ src/components/postpage/QuestionBox/QuestionBox.jsx
@@ -1,21 +1,12 @@
-import { useContext } from 'react';
-import { useGetData } from '../../../hooks/useGetData';
-import { SubjectDataContext } from '../../../contexts/SubjectDataContext';
-
 import FeedCard from '../FeedCard/FeedCard';
 import messages from '../../../images/icons/messages.svg';
 import empty from '../../../images/empty.svg';
 
 import styles from './QuestionBox.module.css';
 
-export default function QuestionBox() {
-  const { id } = useContext(SubjectDataContext);
-  const url = `subjects/${id}/questions/`;
-  const { data, loading } = useGetData(url);
-  if (loading) {
-    return [];
-  }
-  const { count, results } = data;
+export default function QuestionBox({ newData }) {
+  const { count, results } = newData;
+  const sortedResults = results?.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
     <div className={styles.questionBox}>
@@ -25,7 +16,7 @@ export default function QuestionBox() {
       </div>
       {count === 0 ? <img className={styles.emptyIcon} src={empty} alt='questionBoxIcon' /> : null}
       <div className={styles.feedCardList}>
-        {results?.map((result) => <FeedCard key={result.id} data={result} showKebab={true} />) ?? []}
+        {sortedResults?.map((result) => <FeedCard key={result.id} data={result} showKebab={true} />) ?? []}
       </div>
     </div>
   );
