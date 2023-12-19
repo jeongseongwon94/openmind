c6da47a8cd99d8b14c8750c63c2fcb06037aa79e
9ummy9 <ria1005@kakao.com>
2023-12-19 12:22:58 +0900

    Refactor: postpage 수정

---
+++ src/pages/post/PostPage.jsx
@@ -0,0 +1,25 @@
+import { useParams } from 'react-router-dom';
+import { SubjectDataContext } from '../../contexts/SubjectDataContext';
+import { useGetData } from '../../hooks/useGetData';
+
+import MainSection from '../../components/postpage/MainSection/MainSection';
+
+import styles from './PostPage.module.css';
+
+export default function PostPage() {
+  const { id } = useParams();
+  const url = `subjects/${id}/`;
+  const { data, loading } = useGetData(url);
+  if (loading) {
+    return [];
+  }
+
+  return (
+    <div className={styles.postPage}>
+      <SubjectDataContext.Provider value={data}>
+        {/* <Header /> */}
+        <MainSection />
+      </SubjectDataContext.Provider>
+    </div>
+  );
+}
