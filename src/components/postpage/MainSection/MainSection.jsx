e875bf06e0963265b8f380c644635824ccc3ebc6
ccwnc <ccwnc337@gmail.com>
2023-12-20 18:46:09 +0900

    Feat: 무한 스크롤 기능 구현

--- src/components/postpage/MainSection/MainSection.jsx
+++ src/components/postpage/MainSection/MainSection.jsx
@@ -3,6 +3,7 @@ import { useGetData } from '../../../hooks/useGetData';
 import { useIsMobileSize } from '../../../hooks/useIsMobileSize';
 import { SubjectDataContext } from '../../../contexts/SubjectDataContext';
 import { DataChangeDetectionContext } from '../../../contexts/DataChangeDetectionContext';
+import { useIsScrolled } from '../../../hooks/useIsScrolled';
 
 import { axiosBaseURL } from '../../../apis/axiosBaseURL';
 
@@ -14,16 +15,17 @@ import styles from './MainSection.module.css';
 
 export default function MainSection() {
   const { id } = useContext(SubjectDataContext);
-  const url = `subjects/${id}/questions/`;
+  const LIMIT = 3;
+  const page = useIsScrolled();
+  const url = `subjects/${id}/questions/?limit=${LIMIT}`;
+  const newUrl = `subjects/${id}/questions/?limit=${LIMIT}&offset=${LIMIT * page}`;
   const { data, loading } = useGetData(url);
-
-  const isId = localStorage.getItem('id') == id;
-
   const [newData, setNewData] = useState({});
-
   const [dataChangeDetection, setDataChangeDetection] = useState(false);
 
   const [modalOpen, setModalOpen] = useState(false);
+
+  const isId = localStorage.getItem('id') == id;
 
   useEffect(() => {
     if (!loading) {
@@ -47,6 +49,30 @@ export default function MainSection() {
       getData();
     }
   }, [dataChangeDetection]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axiosBaseURL.get(newUrl);
+        setNewData((prevData) => {
+          if (prevData === null) {
+            return response.data;
+          } else {
+            return {
+              ...prevData,
+              results: [...prevData.results, ...response.data.results],
+            };
+          }
+        });
+      } catch {
+        console.error('에러가 발생했습니다!');
+      }
+    };
+
+    if (page) {
+      getData();
+    }
+  }, [page]);
 
   const isMobilSize = useIsMobileSize();
 
