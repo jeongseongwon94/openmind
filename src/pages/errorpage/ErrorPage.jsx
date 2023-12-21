0d389006516063040c4764e3f76b4faa8baa20d2
Chyyni <catharina0409@gmail.com>
2023-12-21 11:14:49 +0900

    refactor: css 색상, 여백 수정

--- src/pages/errorpage/ErrorPage.jsx
+++ src/pages/errorpage/ErrorPage.jsx
@@ -15,7 +15,7 @@ export default function ErrorPage() {
         <img className={styles.errorMessageImage} src={errorMessageImage} alt='길을 잘못 드셨습니다라고 적힌 텍스트' />
         <p className={styles.errorMessage}>고민 해결을 원하신다면 다시 돌아가주세요.</p>
         <div className={styles.btnContainer}>
-          <ButtonBox className='lightButton' text='질문하러가기' handleButtonClick={() => navigateToList('/list')}>
+          <ButtonBox className='lightButton' text='메인페이지로 가기' handleButtonClick={() => navigateToList('/')}>
             <span className={styles.btnText}>
               메인페이지로 가기
               <img className={styles.arrowIcon} src={arrowIcon} alt='오른쪽을 가리키는 화살표 아이콘' />
